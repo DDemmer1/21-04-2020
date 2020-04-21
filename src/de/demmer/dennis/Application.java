b9f0e3076e88788af55b0c338114b8109b9b20ee
DDemmer1 <Dennis.Demmer@web.de>
2020-04-21 11:31:14 +0200

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,8 +1,51 @@
 package de.demmer.dennis;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
 	public static void main(String[] args) {
+		
+
+		Hund hund1 = new Hund("Wautzi",10);
+		Hund hund2 = new Hund("Bello", 5);
+		Hund hund3 = new Hund("Wuffi", 4);
+		Hund hund4 = new Hund("Karlo", 9);
+
+		List<Hund> hundeListe = new ArrayList<>();
+		
+		hundeListe.add(hund1);
+		hundeListe.add(hund2);
+		hundeListe.add(hund3);
+		hundeListe.add(hund4);
+
+		System.out.println("Die Liste hat " + hundeListe.size() + " Einträge");
+		
+	
+		for (Hund hund : hundeListe) {
+			int alter = hund.getAlter();
+			System.out.println("Hund " + hund.getName() + " ist " + alter + " Jahre alt.");
+		}
+		
+		
+		int letzerEintrag = hundeListe.size() -1 ;
+		Hund letzerHund = hundeListe.get(letzerEintrag);
+		
+	
+		System.out.println("Der Name des letzten Hundes ist: " + letzerHund.getName());
+		
+		hundeListe.clear();
+		
+		System.out.println("Die Liste hat " + hundeListe.size() + " Einträge");
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
---
+++ src/de/demmer/dennis/Hund.java
@@ -0,0 +1,35 @@
+package de.demmer.dennis;
+
+public class Hund {
+
+	private String name;
+	private int alter;
+	
+	
+	public Hund(String name, int alter) {
+		super();
+		this.name = name;
+		this.alter = alter;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAlter() {
+		return alter;
+	}
+	public void setAlter(int alter) {
+		this.alter = alter;
+	}
+
+	
+	
+	
+	
+	
+	
+}
